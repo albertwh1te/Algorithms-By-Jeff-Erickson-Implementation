c5ae32f5008f8096fc3448381312e0cdca95e104
markwh1te <iamwh1temark@outlook.com>
2019-05-17 20:03:07 +0800

    🖊 add Half of SubsetSum

---
+++ cplusplus/Bactracking/SubsetSum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool SubsetSum(vector<int> const &X, int T, int start)
+{
+}
+
+int main()
+{
+    cout << "w" << endl;
+    return 0;
+}
